98463dc1f9efe5933322439644aadb10b70556f2
Felippe Nardi <felippe@nardi.me>
2018-07-21 18:09:51 -0300

    Makes animation follow input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import Lottie from './utils/LottieWithAnimationControl'
 import Mouse from './utils/Mouse'
 import face from './face.json'
+import { Motion, spring } from 'react-motion';
+import { NumberValue, StringValue } from 'react-values'
+
+const inputStyle = {
+  fontSize: '30px',
+  margin: 'auto 20px',
+  padding: '15px',
+  width: '400px',
+}
 
 class App extends Component {
   state = {
@@ -16,20 +25,45 @@ class App extends Component {
       <div>
         <Mouse>
           {({x, y }) => (
-            <div style={{width: '400px'}}>
-              <Lottie
-                width={400}
-                height={400}
-                isPaused={this.state.isPaused}
-                options={{
-                  animationData: face,
-                  animationControl: {
-                    'Cursor,Transform,Opacity': 0,
-                    'JoyStkCtrl01,Transform,Position': [x - 200, y - 200],
-                  }
-                }}
-              />
-            </div>
+            <NumberValue>
+              {({value: inputPosition, set: setInputPosition}) => (
+                <div>
+                  <Motion
+                    defaultStyle={{eyesX: 0, eyesX: 0}}
+                    style={{
+                      eyesX: spring(inputPosition - 200),
+                      eyesY: spring(y - 200),
+                    }}>
+                    {({ eyesX, eyesY }) => (
+                      <div style={{width: '400px'}}>
+                        <Lottie
+                          width={400}
+                          height={400}
+                          isPaused={this.state.isPaused}
+                          options={{
+                            animationData: face,
+                            animationControl: {
+                              'JoyStkCtrl01,Transform,Position': [eyesX, 150],
+                            }
+                          }}
+                        />
+                      </div>
+                    )}
+                  </Motion>
+                  <StringValue onChange={(value) => setInputPosition(value.length * 15)}>
+                    {({value, set}) => (
+                      <input
+                      style={inputStyle}
+                      value={value}
+                      onBlur={() => setInputPosition(200)}
+                      onFocus={(e) => set(value)}
+                      onChange={e => set(e.target.value)}
+                    />
+                    )}
+                  </StringValue>
+                </div>
+              )}
+            </NumberValue>
           )}
         </Mouse>
       </div>
