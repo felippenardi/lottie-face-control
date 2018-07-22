02be9d07d508141a89d8b6c32b1a373edfc48f70
Felippe Nardi <felippe@nardi.me>
2018-07-22 11:33:49 -0300

    Make face look straight when leaving input

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Lottie from './utils/LottieWithAnimationControl'
 import Mouse from './utils/Mouse'
 import face from './face.json'
 import { Motion, spring } from 'react-motion';
-import { NumberValue, StringValue } from 'react-values'
+import { ObjectValue, StringValue } from 'react-values'
 
 const inputStyle = {
   fontSize: '30px',
@@ -22,51 +22,45 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Mouse>
-          {({x, y }) => (
-            <NumberValue>
-              {({value: inputPosition, set: setInputPosition}) => (
-                <div>
-                  <Motion
-                    defaultStyle={{eyesX: 0, eyesX: 0}}
-                    style={{
-                      eyesX: spring(inputPosition - 200),
-                      eyesY: spring(y - 200),
-                    }}>
-                    {({ eyesX, eyesY }) => (
-                      <div style={{width: '400px'}}>
-                        <Lottie
-                          width={400}
-                          height={400}
-                          isPaused={this.state.isPaused}
-                          options={{
-                            animationData: face,
-                            animationControl: {
-                              'JoyStkCtrl01,Transform,Position': [eyesX, 150],
-                            }
-                          }}
-                        />
-                      </div>
-                    )}
-                  </Motion>
-                  <StringValue onChange={(value) => setInputPosition(value.length * 15)}>
-                    {({value, set}) => (
-                      <input
-                      style={inputStyle}
-                      value={value}
-                      onBlur={() => setInputPosition(200)}
-                      onFocus={(e) => set(value)}
-                      onChange={e => set(e.target.value)}
-                    />
-                    )}
-                  </StringValue>
+      <ObjectValue defaultValue={{position: 0, isActive: false}}>
+        {({value: reading, assign: assignReading}) => (
+          <div>
+            <Motion
+              defaultStyle={{x: 0, y: 0}}
+              style={{
+                x: spring(reading.isActive ? reading.position - 200 : 0),
+                y: spring(reading.isActive ? 200 : 0),
+            }}>
+              {({ x, y }) => (
+                <div style={{width: '400px'}}>
+                  <Lottie
+                    width={400}
+                    height={400}
+                    isPaused={this.state.isPaused}
+                    options={{
+                      animationData: face,
+                      animationControl: {
+                        'JoyStkCtrl01,Transform,Position': [x, y],
+                      }
+                    }}
+                  />
                 </div>
               )}
-            </NumberValue>
-          )}
-        </Mouse>
-      </div>
+            </Motion>
+            <StringValue onChange={(value) => assignReading({ position: value.length * 15 })}>
+              {({value: text, set: setText}) => (
+                <input
+                  style={inputStyle}
+                  value={text}
+                  onBlur={() => assignReading({ isActive: false })}
+                  onFocus={(e) => assignReading({ isActive: true })}
+                  onChange={e => setText(e.target.value)}
+                />
+              )}
+            </StringValue>
+          </div>
+        )}
+      </ObjectValue>
     );
   }
 }
