4ea1f42097c62d439e99d9845ebf368546eaab10
Felippe Nardi <felippe@nardi.me>
2018-08-07 23:27:45 -0300

    Fix: Demo2 shoudn't contain final result

--- src/Demo2.js
+++ src/Demo2.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import Lottie from 'lottie-react-web'
 import menu from './menu.json'
-import { BooleanValue } from 'react-values'
 
 const Demo2 = () => (
-  <BooleanValue>
-    {({ value, toggle }) => (
-      <div onClick={toggle} style={{width: 100}}>
-        <Lottie
-          direction={value ? -1 : 1}
-          options={{
-            animationData: menu,
-            loop: false,
-          }}
-        />
-      </div>
-    )}
-  </BooleanValue>
+  <div style={{width: 100}}>
+    <Lottie
+      options={{
+        animationData: menu
+      }}
+    />
+  </div>
 )
 
 export default Demo2
