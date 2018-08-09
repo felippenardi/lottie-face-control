c850e3af265d5448bdacb64ba6a5615763766c30
Felippe Nardi <felippe@nardi.me>
2018-08-09 14:41:46 -0300

    Make Demo6 stateless

--- src/Demo6.complete.js
+++ src/Demo6.complete.js
@@ -11,48 +11,44 @@ const inputStyle = {
   width: '400px',
 }
 
-class Demo6Complete extends Component {
-  render() {
-    return (
-      <div style={{width: '400px'}}>
-        <ObjectValue defaultValue={{position: 0, isActive: false}}>
-          {({value: reading, assign: assignReading}) => (
-            <div>
-              <Motion
-                defaultStyle={{x: 0, y: 0}}
-                style={{
-                  x: spring(reading.isActive ? reading.position - 200 : 0),
-                  y: spring(reading.isActive ? 200 : 0),
-                }}>
-                {({ x, y }) => (
-                  <Lottie
-                    options={{
-                      animationData: face,
-                      animationControl: {
-                        'JoyStkCtrl01,Transform,Position': [x, y],
-                      }
-                    }}
-                  />
-                )}
-              </Motion>
+const Demo6Complete = () => (
+  <div style={{width: '400px'}}>
+    <ObjectValue defaultValue={{position: 0, isActive: false}}>
+      {({value: reading, assign: assignReading}) => (
+        <div>
+          <Motion
+            defaultStyle={{x: 0, y: 0}}
+            style={{
+              x: spring(reading.isActive ? reading.position - 200 : 0),
+              y: spring(reading.isActive ? 200 : 0),
+            }}>
+            {({ x, y }) => (
+              <Lottie
+                options={{
+                  animationData: face,
+                  animationControl: {
+                    'JoyStkCtrl01,Transform,Position': [x, y],
+                  }
+                }}
+              />
+            )}
+          </Motion>
 
-              <StringValue onChange={(value) => assignReading({ position: value.length * 15 })}>
-                {({value: text, set: setText}) => (
-                  <input
-                    style={inputStyle}
-                    value={text}
-                    onBlur={() => assignReading({ isActive: false })}
-                    onFocus={(e) => assignReading({ isActive: true })}
-                    onChange={e => setText(e.target.value)}
-                  />
-                )}
-              </StringValue>
-            </div>
-          )}
-        </ObjectValue>
-      </div>
-    );
-  }
-}
+          <StringValue onChange={(value) => assignReading({ position: value.length * 15 })}>
+            {({value: text, set: setText}) => (
+              <input
+                style={inputStyle}
+                value={text}
+                onBlur={() => assignReading({ isActive: false })}
+                onFocus={(e) => assignReading({ isActive: true })}
+                onChange={e => setText(e.target.value)}
+              />
+            )}
+          </StringValue>
+        </div>
+      )}
+    </ObjectValue>
+  </div>
+)
 
 export default Demo6Complete
