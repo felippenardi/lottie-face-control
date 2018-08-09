783caad2a718bcf8b38f26fab575c8c1333f0cb4
Felippe Nardi <felippe@nardi.me>
2018-08-09 15:50:45 -0300

    Setup hot-reload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { AppContainer } from 'react-hot-loader'
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-registerServiceWorker();
+const render = Component => {
+  ReactDOM.render(
+    // Wrap App inside AppContainer
+    <AppContainer>
+      <Router>
+        <App />
+      </Router>
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+
+registerServiceWorker()
+
+render(App)
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App)
+  })
+}
